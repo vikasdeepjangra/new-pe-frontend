6fdd9e38086bf9b3e24db5f29397542b2004a154
Vikasdeep Jangra [UNext] <vikasdeep.jangra@u-next.com>
2023-03-12 15:36:35 +0530

    Frontend File

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,93 @@
+import { OnInit, OnChanges, ViewChild, ElementRef } from '@angular/core';
 import { Component } from '@angular/core';
+import loader from '@monaco-editor/loader';
+import { FileSaverService } from 'ngx-filesaver';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit, OnChanges{
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    loader.config({
+      paths: {
+        vs: './',
+      }
+    });
+  }
+
+  ngOnChanges(){
+    console.log(this.code)
+  }
+
+  fontsize: number = 18;
+  tabsize: number = 4;
+  code: string= '//Type your code here.';
+  fileHandle : any = null;
+  showResultBox = false;
+  showRunning = true;
+  finalResult: any;
+  isfileUploaded: boolean = false;
+  uploadFileName: string;
+
+  editorOptions = {theme: 'vs-dark', 
+                  language: 'cpp', 
+                  renderIndentGuides: true, 
+                  fontSize: this.fontsize, 
+                  tabSize: this.tabsize,
+                  wordWrap: "on"};
+
+  async uploadFileUsingFileSystemAPI(){
+    [this.fileHandle] = await (window as any).showOpenFilePicker();
+    const file: File = await this.fileHandle.getFile();
+    const contents = await file.text();
+    if(this.fileHandle != null){
+      this.isfileUploaded = true;
+      this.uploadFileName = this.fileHandle.name;
+      this.finalResult = "";
+      this.showResultBox = false;
+    }
+    this.code = contents;
+  }
+
+  async saveFileUsingFileSystemAPI(){
+    const contents = this.code;
+    const writable = await this.fileHandle.createWritable();
+    await writable.write(contents);
+    await writable.close();
+  }
+
+  async resetEditor(){
+    this.fileHandle = null;
+    this.code = '//Type your code here.';
+    this.finalResult = "";
+    this.showResultBox = false;
+    this.uploadFileName = ""
+  }
+
+  async compileRunCode(){
+    this.showRunning = true;
+    this.showResultBox = true;
+    this.finalResult = "";
+    const x = { data: "Hello"};
+    const data = await fetch('http://localhost:5000/saveFileAndCompile', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ code: this.code })
+    })
+    this.showRunning = false;
+    console.log(data);
+    data.text().then((x) => {
+      this.finalResult = x;
+      console.log("The Output is: ", this.finalResult);
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MonacoEditorModule } from 'ngx-monaco-editor';
+import { FormsModule } from '@angular/forms';
+import { FileSaverModule } from 'ngx-filesaver';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MonacoEditorModule.forRoot(),
+    FormsModule,
+    FileSaverModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
